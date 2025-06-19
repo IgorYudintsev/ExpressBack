125e934b0aeb679a6d6fbe9e2f0f142a8b98abfd
IgorYudintsev <kakvospitat@gmail.com>
2025-06-19 13:43:44 +0300

    30-variables-dotenv

--- src/db/booksDb.ts
+++ src/db/booksDb.ts
@@ -1,8 +1,10 @@
 import { MongoClient } from 'mongodb';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
- const mongoURI = process.env.MONGODB_URI
+ // const mongoURI = process.env.MONGODB_URI
+const mongoURI = "mongodb://mongo:dbTBMqHbexqkrpeLnCzRNXBZzTUNyibm@interchange.proxy.rlwy.net:58173/kiberRus?authSource=admin";
+
 
 if (process.env.NODE_ENV !== 'production') {
     console.log("🔥 MONGODB_URI =", process.env.MONGODB_URI);
